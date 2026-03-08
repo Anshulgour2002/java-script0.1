e5b64fc41951d87bce9353ce6ac8175aa01b28f0
Anshulgour2002 <anshulgour968@gmail.com>
2026-03-09 00:06:29 +0530

    change

--- style.js
+++ style.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-
-// debugger;
 
 // var x=10;
 // var y=20;
@@ -31,8 +27,8 @@
 
 let x = 5;
 let y = x++;
-=======
-var x=90;
+
+
 console.log(x)
 console.log("Hello Anshul");
->>>>>>> 2869b26d348918bd1ec79107c994472c333de698
+
