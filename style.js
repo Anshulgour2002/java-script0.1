8f07a561dd6cfc349bd84eff010cc0d8878988ff
Anshulgour2002 <anshulgour968@gmail.com>
2026-03-08 23:55:51 +0530

    add

---
+++ style.js
@@ -0,0 +1,32 @@
+
+
+// debugger;
+
+// var x=10;
+// var y=20;
+// x=20
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x*y);
+
+
+// console.log(x>y)
+// console.log(x<y)
+// console.log(x==y)
+// console.log(x>y)
+
+// console.log(x>y && x==y)
+// console.log(x>y || x==y)
+
+
+// let x = 5;
+// let y = ++x;
+
+// console.log(x); 
+
+
+let x = 5;
+let y = x++;
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+
+
+// debugger;
+
+// var x=10;
+// var y=20;
+// x=20
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x*y);
+
+
+// console.log(x>y)
+// console.log(x<y)
+// console.log(x==y)
+// console.log(x>y)
+
+// console.log(x>y && x==y)
+// console.log(x>y || x==y)
