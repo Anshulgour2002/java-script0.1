cb7a4d9507d7f63b608dd052613d4a803d842424
Anshulgour2002 <anshulgour968@gmail.com>
2026-03-12 23:37:03 +0530

    add this file

---
+++ class7-object/object.js
@@ -0,0 +1,15 @@
+let obj={
+    id:10,
+    name2:"Raj",
+    101:1000,
+    "u id":'c2115',
+    "e-id":105,
+
+}
+console.log(obj[101])
+console.log(obj["e-id"])
+
+console.log(obj["u id"])
+console.log(obj.name2)
+console.log(obj["id"])
+console.log(obj.id)
---
+++ class7-object/object2.js
@@ -0,0 +1,11 @@
+let ob2={
+    id:10,
+    username:"Mayur",
+    hello:function(){
+        console.log("Hllo")
+    }
+}
+console.log(ob2.hello(
+
+    
+))
