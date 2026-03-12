86bb6c216fb607b32ed8c1df67e744bba560ee4f
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-03-12 10:10:40 +0530

    Add

---
+++ class4-switch/switch.js
@@ -0,0 +1,23 @@
+let dayno = prompt();
+dayno=Number(dayno);
+switch (dayno) {
+    case 1:
+        console.log("Monday")
+        break;
+  case 2:
+        console.log("Tuesday")
+        break;
+  case 3:
+        console.log("wednesday")
+        break;
+  case 4:
+        console.log("Thursday")
+        break;
+  case 5:
+        console.log("friday")
+        break;
+
+    default:
+        console.log("Please enter the valid day")
+        break;
+}
